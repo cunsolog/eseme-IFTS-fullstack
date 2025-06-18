002f0320112b206d7b5f9eea033c25d1cc26e7ca
Giuseppe Cunsolo <giuseppe.cunsolo.posta@gmail.com>
2025-06-18 14:14:38 +0200

    errore codcie metodo astratto

--- src/main/java/FAVoloso/model/CampoSportivo.java
+++ src/main/java/FAVoloso/model/CampoSportivo.java
@@ -5,10 +5,5 @@ public abstract class CampoSportivo {
 
     //SUGGERIMENTO: Questa metodo permette di mostrare il
     // campo "tipo" (Discriminator) creato nel DB da JPA per gestire il polimorfismo.
-    @Override
-    public String getTipo() {
-        return "TENNIS";
-    }
-
-
+    public abstract String getTipo();
 }
