035b34d19ca6bcb9f319f51445faf5076a4bb015
domenicoanania <50619594+domenicoanania@users.noreply.github.com>
2025-06-21 23:58:32 +0200

    Update PrenotazioneResponse.java

--- src/main/java/FAVoloso/dto/PrenotazioneResponse.java
+++ src/main/java/FAVoloso/dto/PrenotazioneResponse.java
@@ -1,1 +1,21 @@
+package FAVoloso.dto;
 
+import FAVoloso.model.Prenotazione;
+
+import java.util.List;
+
+public class PrenotazioneResponse {
+    Prenotazione prenotazione;
+
+
+    public PrenotazioneResponse(Prenotazione prenotazione) {
+        this.prenotazione = prenotazione;
+
+    }
+
+    public Prenotazione getPrenotazione() {
+        return prenotazione;
+    }
+
+
+}
